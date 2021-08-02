ce38ed83323030be40e285b6a25a801b76beea22
redricasa <fredeb26@gmail.com>
2021-08-02 14:53:05 -0700

    fizzbuzz files

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+// add your Class name
+public class FizzBuzz {;
+// change according to if it is a test file or main file
+    public String fizzBuzz(int number){
+        
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+// add your Class name
+public class  TestFizzBuzz {;
+// change according to if it is a test file or main file
+    public static void main(String[] args){
+        FizzBuzz tester = new FizzBuzz();
+
+        System.out.println(tester.fizzBuzz(9) );
+        System.out.println(tester.fizzBuzz(10) );
+        System.out.println(tester.fizzBuzz(15) );
+        System.out.println(tester.fizzBuzz(16) );
+        System.out.println(tester.fizzBuzz(2) );
+    }
+}
